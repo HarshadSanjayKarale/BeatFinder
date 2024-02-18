4aabbccd2806e7ab2a5ecfb77593496e01d49ae0
= <=>
2024-02-18 15:47:26 +0530

    added mongo

--- backend/db/firestore.js
+++
@@ -1,9 +0,0 @@
-const firebaseAdmin = require('firebase-admin');
-const serviceAccount = require('../secret/firebaseConfig.js');
-
-// Initialize Firebase Admin SDK
-const db = firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount)
-}).firestore();
-
-module.exports = db;
--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-// routes/index.js
-
-const express = require('express');
-const router = express.Router();
-const authRoutes = require('./authRoutes');
-
-router.use('/auth', authRoutes);
-
-module.exports = router;
